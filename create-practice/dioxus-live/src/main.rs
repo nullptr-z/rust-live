7d05e3eccaeaaa83202a4b6ff0da8848bb80105e
WmW <zhouzheng0646@qq.com>
2023-04-19 23:10:41 +0800

    2023-04-19 23:10:34

--- create-practice/dioxus-live/src/main.rs
+++ create-practice/dioxus-live/src/main.rs
@@ -11,5 +11,3 @@ fn app(cx: Scope) -> Element {
         h1 { "High-Five counter: {count}" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
-    ))
-}
