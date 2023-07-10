10b5184b4f0fe2a8348550fa2f800724ecf40ee6
WmW <zhouzheng0646@qq.com>
2023-07-11 00:32:12 +0800

    2023-07-11

--- kv-server/src/network/frame.rs
+++ kv-server/src/network/frame.rs
@@ -6,8 +6,8 @@ pub trait FrameCoder
 where
     Self: Sized + Default,
 {
-    // encode Message to Frame
+    // encode Message into a Frame, and write it into buf
     fn frame_decode(&self, buf: &mut BytesMut) -> Result<(), KvError>;
-    // decode Frame to Message
+    // decode Frame into Message
     fn frame_encode(buf: &mut BytesMut) -> Result<Self, KvError>;
 }
