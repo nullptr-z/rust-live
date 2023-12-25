e43a988e4911bbf627ecd2866fbaa9812af03ad7
WmW <zhouzheng0646@qq.com>
2023-12-25 23:27:33 +0800

    Storage and MemTable

--- kv-db/src/lib.rs
+++ kv-db/src/lib.rs
@@ -1,1 +1,3 @@
+pub mod error;
 pub mod pb;
+pub mod storage;
---
+++ kv-db/src/storage/memory.rs
@@ -0,0 +1,80 @@
+use crate::{
+    error::KvError,
+    pb::abi::{Kvpair, Value},
+};
+use dashmap::{mapref::one::Ref, DashMap};
+
+use super::Storage;
+
+#[derive(Debug, Default, Clone)]
+pub struct MemTable {
+    tables: DashMap<String, DashMap<String, Value>>,
+}
+
+impl MemTable {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    fn get_or_create_table(&self, name: impl Into<String>) -> Ref<String, DashMap<String, Value>> {
+        let names: String = name.into();
+        match self.tables.get(&names) {
+            Some(table) => table,
+            None => {
+                let entry = self.tables.entry(names).or_default();
+                entry.downgrade()
+            }
+        }
+    }
+}
+
+impl Storage for MemTable {
+    fn get(
+        &self,
+        table: impl Into<String>,
+        key: impl Into<String>,
+    ) -> Result<Option<Value>, KvError> {
+        let table = self.get_or_create_table(table);
+        Ok(table.get(&key.into()).map(|v| v.value().clone()))
+    }
+
+    fn set(
+        &self,
+        table: impl Into<String>,
+        key: impl Into<String>,
+        value: Value,
+    ) -> Result<Option<Value>, KvError> {
+        let table = self.get_or_create_table(table);
+        Ok(table.insert(key.into(), value.into()))
+    }
+
+    fn contains(&self, table: impl Into<String>, key: impl Into<String>) -> Result<bool, KvError> {
+        let table = self.get_or_create_table(table);
+        Ok(table.contains_key(&key.into()))
+    }
+
+    fn del(
+        &self,
+        table: impl Into<String>,
+        key: impl Into<String>,
+    ) -> Result<Option<Value>, KvError> {
+        let table = self.get_or_create_table(table);
+        Ok(table.remove(&key.into()).map(|(_, v)| v))
+    }
+
+    fn get_all(&self, table: impl Into<String>) -> Result<Vec<Kvpair>, KvError> {
+        let table = self.get_or_create_table(table);
+        let result: Vec<Kvpair> = table
+            .iter()
+            .map(|m| Kvpair::new(m.key(), m.value().clone()))
+            .collect();
+        Ok(result)
+    }
+
+    fn get_iter(
+        &self,
+        table: impl Into<String>,
+    ) -> Result<Box<dyn Iterator<Item = Kvpair>>, KvError> {
+        todo!()
+    }
+}
