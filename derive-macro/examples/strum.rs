00433168777566c213fc71cbcc83791ea700304d
zhouzheng <zhouzheng.0646@bytedance.com>
2024-05-23 20:49:16 +0800

    lerna derive macro

---
+++ derive-macro/examples/strum.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use strum::{
+    EnumCount, EnumDiscriminants, EnumIs, EnumIter, EnumMessage, EnumString, IntoEnumIterator,
+    VariantNames,
+};
+
+#[derive(
+    Debug, EnumString, EnumCount, EnumIter, EnumDiscriminants, EnumIs, EnumMessage, VariantNames,
+)]
+enum MyEnum {
+    A,
+    B,
+    C,
+    D,
+}
+
+fn main() -> Result<()> {
+    MyEnum::VARIANTS.iter().for_each(|v| {
+        println!("【 v 】==> {:?}", v);
+    });
+    MyEnum::iter().for_each(|v| {
+        println!("【 v 】==> {:?}", v);
+    });
+    Ok(())
+}
