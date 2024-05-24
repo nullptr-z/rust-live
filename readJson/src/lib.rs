510bbea80723a98074beb860efede508c68f1305
zhouzheng <zhouzheng.0646@bytedance.com>
2024-05-24 20:09:32 +0800

    read json

--- readJson/src/lib.rs
+++ readJson/src/lib.rs
@@ -1,1 +1,20 @@
+// const length: *mut usize = std::ptr::null_mut();
 
+#[no_mangle]
+pub extern "C" fn gen_array(a: i32, b: i32) -> *const i32 {
+    let v: Vec<i32> = (a..b).rev().collect();
+    let ptr = v.as_ptr();
+    std::mem::forget(v); // 避免 Rust 将内存释放
+                         // unsafe { *length = len };
+    ptr
+}
+
+// #[no_mangle]
+// pub extern "C" fn gen_array_len() -> usize {
+//     unsafe { *length }
+// }
+
+#[no_mangle]
+pub extern "C" fn diff(a: i32, b: i32) -> i32 {
+    a - b
+}
